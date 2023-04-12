a0fec98290495e15cf891291ec80437359f3fca8
Ramyunman <100078669+Ramyunman@users.noreply.github.com>
2023-04-12 10:30:04 +0900

    Initial commit

---
+++ src/main/java/com/sonfirm/example/SonfirmApplication.java
@@ -0,0 +1,13 @@
+package com.sonfirm.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonfirmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonfirmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonfirm/example/SonfirmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonfirm.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonfirmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
