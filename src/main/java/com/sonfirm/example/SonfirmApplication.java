ab48c4387ac7b0526bb2d1ec7e0b4f092002ba69
Ramyunman <100078669+Ramyunman@users.noreply.github.com>
2023-05-17 17:16:32 +0900

    230517 survey

--- src/main/java/com/sonfirm/example/SonfirmApplication.java
+++ src/main/java/com/sonfirm/example/SonfirmApplication.java
@@ -1,14 +1,14 @@
-package com.sonfirm.example;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SonfirmApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SonfirmApplication.class, args);
-		System.out.println("Hello World~!");
-	}
-
-}
+//package com.sonfirm.example;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class SonfirmApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SonfirmApplication.class, args);
+//		System.out.println("Hello World~!");
+//	}
+//
+//}
